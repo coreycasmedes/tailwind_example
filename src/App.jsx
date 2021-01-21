197e52eaf88a3487ee666b62077a0ba3c2b5df6c
Corey Casmedes <ccasmedes3@gmail.com>
2021-01-20 21:27:57 -0600

    testing tailwind ui components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Dropwndown from './Dropdown';
+import Navbar from './Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
